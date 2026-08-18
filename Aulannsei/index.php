8bc78a502c69f7686ab92e2637b1eeb7a1dcfdf7
Nicolly <nicolly.ofontes@aluno.edu.es.gov.br>
2026-08-18 10:54:34 -0300

    Update index.php

--- Aulannsei/index.php
+++ Aulannsei/index.php
@@ -6,16 +6,16 @@
 </head>
 <body>
     <?php
-    $preco= 120;
-    $quantidade= 3;
-    $desconto= 30;
-    $parcelas= 2;
-
-    echo "Valor total da compra: " .($preco*$quantidade)<br>;
-    echo "Valor da compra com desconto: " .(($preco*$quantidade)-$desconto)<br>;
-    echo "Valor de cada parcela: " .((($preco*$quantidade)-$desconto)/$parcelas)<br>;
-    echo "Valor da compra caso seja aplicada uma taxa de R$15,00: " .(((($preco*$quantidade)-$desconto)/$parcelas)+15)<br>;
-    echo "Valor final: " .((((($preco*$quantidade)-$desconto)/$parcelas)+15)%2);
+        $preco= 120;
+        $quantidade= 3;
+        $desconto= 30;
+        $parcelas= 2;
+    
+        echo "Valor total da compra: " .($preco*$quantidade)<br>;
+        echo "Valor da compra com desconto: " .(($preco*$quantidade)-$desconto)<br>;
+        echo "Valor de cada parcela: " .((($preco*$quantidade)-$desconto)/$parcelas)<br>;
+        echo "Valor da compra caso seja aplicada uma taxa de R$15,00: " .(((($preco*$quantidade)-$desconto)/$parcelas)+15)<br>;
+        echo "Valor final: " .((((($preco*$quantidade)-$desconto)/$parcelas)+15)%2);
     ?>
 </body>
     
